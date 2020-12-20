9d8f4b607581a8b579b05f59d5709bfe8c605f1c
jongwon <54282927+loosie@users.noreply.github.com>
2020-12-20 15:50:31 +0900

    R_S_Y_C #7 디테일 페이지 (#2 side)

--- Clone/React_Study_Youtube_Clone/client/src/components/views/VideoDetailPage/VideoDetailPage.js
+++ Clone/React_Study_Youtube_Clone/client/src/components/views/VideoDetailPage/VideoDetailPage.js
@@ -1,6 +1,7 @@
 import React, { useState, useEffect} from 'react'
 import { Row, Col, List, Avatar } from 'antd';
 import Axios from 'axios';
+import SideVideo from './Sections/SideVideo';
 
 function VideoDetailPage(props) {
 
@@ -11,7 +12,7 @@ function VideoDetailPage(props) {
     
 
     useEffect(() => {
-        Axios.post('/api/video/getVideoDetail', variable)
+        Axios.post('/api/video/getVideoDetail', variable) // post : variable (videoId) > 서버에게 전송
         .then(res=> {
             if(res.data.success){
                 console.log(res.data);
@@ -45,7 +46,7 @@ function VideoDetailPage(props) {
                     </div>
                 </Col>
                 <Col lg={6} xs={24}>
-                    Side Videos
+                    <SideVideo />
                 </Col>
             </Row>
         )
