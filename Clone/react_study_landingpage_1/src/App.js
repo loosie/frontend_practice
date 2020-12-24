99744d01a93695c7cd8936b6ebfd97278c4367a7
jongwon <54282927+loosie@users.noreply.github.com>
2020-12-24 20:25:42 +0900

    R_S_L_1 #2 create Button Component & navbar

--- Clone/react_study_landingpage_1/src/App.js
+++ Clone/react_study_landingpage_1/src/App.js
@@ -1,10 +1,18 @@
 import './App.css';
+import Navbar from './components/Navbar'
+import {BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 
 function App() {
   return (
-    <div className="App">
-      YO
-    </div>
+    <>
+      <Router>
+        <Navbar />
+        <Switch>
+          <Route path='/' exact />
+        </Switch>
+      </Router>
+      
+    </>
   );
 }
 
