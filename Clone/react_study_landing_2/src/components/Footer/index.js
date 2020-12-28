4a610712c54e2abf2ecbb891d5def11a8ae8d099
jongwon <54282927+loosie@users.noreply.github.com>
2020-12-29 04:08:27 +0900

    R_S_L_2 #6 Create Footer

---
+++ Clone/react_study_landing_2/src/components/Footer/index.js
@@ -0,0 +1,54 @@
+import React from 'react'
+
+const Footer = () => {
+    return (
+        <FooterContainer>
+            <FooterWrap>
+                <FooterLinksContainer>
+                    <FooterLinksWrapper>
+
+                        <FooterLinkItems>
+                            <FooterLinkTitle>About us</FooterLinkTitle>
+                                <FooterLink to="/siginin">How it works</FooterLink>
+                                <FooterLink to="/siginin">Testimonials</FooterLink>
+                                <FooterLink to="/siginin">Investors</FooterLink>
+                                <FooterLink to="/siginin">Terms of Service</FooterLink>
+                        </FooterLinkItems>
+
+                        <FooterLinkItems>
+                            <FooterLinkTitle>About us</FooterLinkTitle>
+                                <FooterLink to="/siginin">How it works</FooterLink>
+                                <FooterLink to="/siginin">Testimonials</FooterLink>
+                                <FooterLink to="/siginin">Investors</FooterLink>
+                                <FooterLink to="/siginin">Terms of Service</FooterLink>
+                        </FooterLinkItems>
+
+                    </FooterLinksWrapper>
+
+                    <FooterLinksWrapper>
+                        
+                        <FooterLinkItems>
+                            <FooterLinkTitle>About us</FooterLinkTitle>
+                                <FooterLink to="/siginin">How it works</FooterLink>
+                                <FooterLink to="/siginin">Testimonials</FooterLink>
+                                <FooterLink to="/siginin">Investors</FooterLink>
+                                <FooterLink to="/siginin">Terms of Service</FooterLink>
+                        </FooterLinkItems>
+
+                        <FooterLinkItems>
+                            <FooterLinkTitle>About us</FooterLinkTitle>
+                                <FooterLink to="/siginin">How it works</FooterLink>
+                                <FooterLink to="/siginin">Testimonials</FooterLink>
+                                <FooterLink to="/siginin">Investors</FooterLink>
+                                <FooterLink to="/siginin">Terms of Service</FooterLink>
+                        </FooterLinkItems>
+
+                    </FooterLinksWrapper>
+                </FooterLinksContainer>
+            </FooterWrap>
+            
+        </FooterContainer>
+    )
+}
+
+export default Footer;
--- Clone/react_study_landing_2/src/pages/index.js
+++ Clone/react_study_landing_2/src/pages/index.js
@@ -1,4 +1,5 @@
 import React, {useState} from 'react'
+import Footer from '../components/Footer'
 import HeroSection from '../components/HeroSection'
 import InfoSection from '../components/InfoSection'
 import { HomeObjOne, HomeObjTwo, HomeObjThree } from '../components/InfoSection/Data'
@@ -29,6 +30,7 @@ const Home = () => {
             <Services />
             <InfoSection {...HomeObjThree} pic={pic3} />
 
+            <Footer />
 
         </>
     )
