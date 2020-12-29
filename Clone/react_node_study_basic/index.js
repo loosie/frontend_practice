99503606bfc01ed6c0cc4a054e62f66c7d40b926
jongwon <54282927+loosie@users.noreply.github.com>
2020-12-29 23:20:13 +0900

    R_N_S_B #2 mongoDB Connection

--- Clone/react_node_study_basic/index.js
+++ Clone/react_node_study_basic/index.js
@@ -2,6 +2,15 @@ const express = require('express')
 const app = express()
 const port = 5000
 
+const mongoose = require('mongoose')
+
+mongoose.connect('mongodb+srv://loosie2:whddnjs!112@boiler-plate.eigsa.mongodb.net/<dbname>?retryWrites=true&w=majority', {
+    useNewUrlParser: true, useUnifiedTopology: true, userCreateIndex: true, useFindAndModify: false
+}).then(() => console.log('MongoDB connected...'))
+    .catch(err => console.log(err));
+
+
+
 app.get('/', (req, res) => {
   res.send('Hello World!')
 })
