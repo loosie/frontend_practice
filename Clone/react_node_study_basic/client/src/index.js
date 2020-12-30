9c6f095396906d2f940be0f9a9bd98f1b93bfae6
jongwon <54282927+loosie@users.noreply.github.com>
2020-12-31 00:39:22 +0900

    R_N_S_B #18 Redux setup(promise, thunk, devtools)

--- Clone/react_node_study_basic/client/src/index.js
+++ Clone/react_node_study_basic/client/src/index.js
@@ -2,15 +2,33 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
+import { Provider } from 'react-redux';
 import reportWebVitals from './reportWebVitals';
-
 import 'antd/dist/antd.css';
+import { applyMiddleware } from 'redux';
+import promiseMiddleware from 'redux-promise';
+import ReduxThunk from 'redux-thunk';
+import Reducer from './_reducers';
+
+//promise, thunk를 해서 store가 promise, func형태 object도 받을 수 있게 해줌 (원래는 그냥 plian object만 받을 수 있음)
+const  createStoreWithMiddleware = applyMiddleware(promiseMiddleware, ReduxThunk)(createStore)
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
+
+
+    <Provider
+      store={createStoreWithMiddleware(Reducer,
+        // chrome 확장프로그램 redux devtools와 연결
+          window.__REDUX_DEVTOOLS_EXTENSION__ &&
+          window.__REDUX_DEVTOOLS_EXTENSION__()
+        )}
+    >
+      <App />
+    </Provider>
+    
+    
+    
+    ,  document.getElementById('root')
 );
 
 // If you want to start measuring performance in your app, pass a function
