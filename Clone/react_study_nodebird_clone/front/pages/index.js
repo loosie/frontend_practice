060078f341f30cd6f1616b66306cffd53185e3a6
jongwon <54282927+loosie@users.noreply.github.com>
2021-01-01 23:49:01 +0900

    r_s_n_c #0 create next.js

---
+++ Clone/react_study_nodebird_clone/front/pages/index.js
@@ -0,0 +1,16 @@
+// import React from 'react' > next는 생략가능
+
+import AppLayout from '../components/AppLayout'
+
+
+const Home = () => {
+    return (
+        <AppLayout>
+            <div>
+                Hello next!
+            </div>
+        </AppLayout>
+    )
+}
+
+export default Home
---
+++ Clone/react_study_nodebird_clone/front/pages/profile.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import AppLayout from '../components/AppLayout'
+
+const Signup = () => {
+    return (
+        <AppLayout>
+            <div>
+                내 프로필
+            </div>
+        </AppLayout>
+    )
+}
+
+export default Signup
