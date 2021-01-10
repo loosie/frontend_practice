d48d534df8c8f9dd04ad087715e5346cac5b9c6b
jongwon <54282927+loosie@users.noreply.github.com>
2021-01-11 01:03:56 +0900

    Revert "r_s_n_c #19 CSS(Styled-Components) SSR적용 ( babel-plugin-styled-components )"
    This reverts commit b56289d5353e7ebde803939e8a49fc4133148887.

--- Clone/react_study_landing_2/src/components/HeroSection/index.js
+++ Clone/react_study_landing_2/src/components/HeroSection/index.js
@@ -3,7 +3,7 @@ import { HeroBg, HeroContainer, VideoBg,
         HeroContent, HeroH1, HeroP, HeroBtnWrapper,
         ArrowForward, ArrowRight } from './HeroElements'
 import { Button } from "../ButtonElement";
-import Video from '../../videos/video3.mp4'
+import Video from '../../videos/video.mp4'
 
 const HeroSection = () => {
     const [hover, setHover] = useState(false);
@@ -18,14 +18,14 @@ const HeroSection = () => {
                 <VideoBg autoPlay loop muted src={Video} type='video/mp4' />
             </HeroBg>
             <HeroContent>
-                {/* <HeroH1>
+                <HeroH1>
                     Paint your own dot on the world
                 </HeroH1>
                 <HeroP>
                     아티스트 브랜딩 콘텐츠 커뮤니티 블루닷에 오신 것을 환영합니다!
-                </HeroP> */}
+                </HeroP>
                 <HeroBtnWrapper>
-                    {/* <Button to="signup" 
+                    <Button to="signup" 
                     onMouseEnter={onHover}
                     onMouseLeave={onHover}
                     primary='true'
@@ -39,7 +39,7 @@ const HeroSection = () => {
                     offset={-80} 
                     >
                     Get Started {hover? <ArrowForward /> : <ArrowRight />}
-                    </Button> */}
+                    </Button>
                 </HeroBtnWrapper>
             </HeroContent>
         </HeroContainer>
