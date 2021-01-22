4c7c68950d4f5d76ee7dbd2be5f39097ebb1d4d5
jongwon <54282927+loosie@users.noreply.github.com>
2021-01-22 21:19:47 +0900

    #25 immer 라이브러리 사용 (불변성)

--- Clone/react_study_nodebird_clone/front/reducers/user.js
+++ Clone/react_study_nodebird_clone/front/reducers/user.js
@@ -1,3 +1,4 @@
+import produce from 'immer';
 
 export const initialState = {
     followLoading: false, // 팔로우 시도중
@@ -76,111 +77,91 @@ export const logoutRequestAction = () => {
 
 
 const reducer = (state = initialState, action) =>{
+    return produce(state, (draft) => {
     switch (action.type){
         case LOG_IN_REQUEST:
-            console.log('reducer login');
-            return{
-                ...state,
-                logInLoading: true,
-                logInError: null,
-                logInDone: false,
-                };
-        
+            draft.logInLoading = true;
+            draft.logInError = null;
+            draft.logInDone = false;
+            break;
         case LOG_IN_SUCCESS:
-            return{
-                ...state,
-                logInLoading: false,
-                logInDone: true,
-                me: dummyUser(action.data),
-                };
+            draft.logInLoading = false;
+            draft.me = dummyUser(action.data);
+            draft.logInDone = true;
+            break;
         case LOG_IN_FAILURE:
-            return{
-                ...state,
-                logInLoading: false,
-                logInError: action.error,
-                };
+            draft.logInLoading = false;
+            draft.logInError = action.error;
+            break;
 
         case LOG_OUT_REQUEST:
-            return{
-                ...state,
-                logOutLoading: true,
-                logOutError: null,
-                logOutDone: false,
-                };
+            draft.logOutLoading = true;
+            draft.logOutError = null;
+            draft.logOutDone = false;
+            break;
         case LOG_OUT_SUCCESS:
-            return{
-                ...state,
-                logOutLoading: false,
-                logOutDone: true,
-                me: null
-                };
+            draft.logOutLoading = false;
+            draft.logOutDone = true;
+            draft.me = null;
+            break;
         case LOG_OUT_FAILURE:
-            return{
-                ...state,
-                logOutLoading: false,
-                logOutError: action.error,
-                };
+            draft.logOutLoading = false;
+            draft.logOutError = action.error;
+            break;
 
         case SIGN_UP_REQUEST:
-            return{
-                ...state,
-                signUpLoading: true,
-                signUpError: null,
-                signUpDone: false,
-                };
+            draft.signUpLoading = true;
+            draft.signUpError = null;
+            draft.signUpDone = false;
+            break;
         case SIGN_UP_SUCCESS:
-            return{
-                ...state,
-                signUpLoading: false,
-                signUpDone: true,
-                };
+            draft.signUpLoading = false;
+            draft.signUpDone = true;
+            break;
         case SIGN_UP_FAILURE:
-            return{
-                ...state,
-                signUpLoading: false,
-                signUpError: action.error,
-                };
+            draft.signUpLoading = false;
+            draft.signUpError = action.error;
+            break;
 
          case CHANGE_NICKNAME_REQUEST:
-            return{
-                ...state,
-                changeNicknameLoading: true,
-                changeNicknameError: null,
-                changeNicknameDone: false,
-                };
+            draft.changeNicknameLoading = true;
+            draft.changeNicknameError = null;
+            draft.changeNicknameDone = false;
+            break;
         case CHANGE_NICKNAME_SUCCESS:
-            return{
-                ...state,
-                changeNicknameLoading: false,
-                changeNicknameDone: true,
-                };
+            draft.changeNicknameLoading = false;
+            draft.changeNicknameDone = true;
+            break;
         case CHANGE_NICKNAME_FAILURE:
-            return{
-                ...state,
-                changeNicknameLoading: false,
-                changeNicknameError: action.error,
-                };
+            draft.changeNicknameLoading = false;
+            draft.changeNicknameError = action.error;
+            break;
         
         case ADD_POST_TO_ME:
-            return {
-                ...state,
-                me: {
-                    ...state.me,
-                    Posts: [{ id: action.data}, ...state.me.Posts],
-                },
-            }
+            draft.me.Posts.unshift({ id: action.data });
+            break;
+            // return {
+            //     ...state,
+            //     me: {
+            //         ...state.me,
+            //         Posts: [{ id: action.data}, ...state.me.Posts],
+            //     },
+            // }
 
         case REMOVE_POST_OF_ME:
-            return {
-                ...state,
-                me: {
-                    ...state.me,
-                    Posts: state.me.Posts.filter((v) => v.id !== action.data),
-                },
-            }
+            draft.me.Posts = draft.me.Posts.filter((v) => v.id !== action.data); //성능 문제 생기면 그 때 unshift로 바꿈
+            break; 
+            // return {
+            //     ...state,
+            //     me: {
+            //         ...state.me,
+            //         Posts: state.me.Posts.filter((v) => v.id !== action.data),
+            //     },
+            // }
         default:
             return state;
-    }
+        }
+    });
 }
 
 
