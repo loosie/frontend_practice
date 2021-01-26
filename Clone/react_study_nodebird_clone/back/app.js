c916f38509e534dd21aa9cf8ed32a9669e78e1c1
jongwon <54282927+loosie@users.noreply.github.com>
2021-01-27 03:38:31 +0900

    #31 node signup 초기 setting

--- Clone/react_study_nodebird_clone/back/app.js
+++ Clone/react_study_nodebird_clone/back/app.js
@@ -1,5 +1,6 @@
 const express = require('express')
 const postRouter = require('./routes/post');
+const userRouter = require('./routes/user');
 const db = require('./models');
 const app = express();
 db.sequelize.sync()
@@ -8,12 +9,18 @@ db.sequelize.sync()
     })
     .catch(console.error);
 
+// ---router보다 위에 설정----
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+// -----------------------
+
 app.get('/', (req, res) =>{
     res.send('hello express');
 });
 
 // router
 app.use('/post', postRouter);
+app.use('/user', userRouter);
 
 app.listen(3065, ()=>{
     console.log("서버 실행 중");
