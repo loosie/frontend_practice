0cf18f6d5ce7c3c8a908df70cf4b4d9ff61a65f2
jongwon <54282927+loosie@users.noreply.github.com>
2021-01-28 03:20:09 +0900

    #33 node passport 로그인 구현

--- Clone/react_study_nodebird_clone/back/app.js
+++ Clone/react_study_nodebird_clone/back/app.js
@@ -4,6 +4,7 @@ const cors = require('cors');
 const postRouter = require('./routes/post');
 const userRouter = require('./routes/user');
 const db = require('./models');
+const passportConfig = require('./passport');
 
 const app = express();
 db.sequelize.sync()
@@ -12,6 +13,7 @@ db.sequelize.sync()
     })
     .catch(console.error);
 
+passportConfig();
 
 app.use(cors({
     origin: '*',
--- Clone/react_study_nodebird_clone/back/routes/user.js
+++ Clone/react_study_nodebird_clone/back/routes/user.js
@@ -1,8 +1,32 @@
 const express = require('express');
 const bcrypt = require('bcrypt');
 const { User } = require('../models');
+const passport = require('passport');
 
 const router = express.Router();
+
+router.post('/login', (req,res, next)=> {
+    passport.authenticate('local', (err, user, info) => {
+        if(err){
+            console.error(err);
+            return next(err);
+        }
+        if(info){
+            return res.status(401).send(info.reason);
+        }
+        
+        // passport.login
+        return req.login(user, async(loginErr) => {
+            if(loginErr){
+                console.error(loginErr);
+                return next(loginErr);
+            }
+
+            return res.json(user);
+        })
+    })(req, res, next);
+});
+
 
 router.post('/', async(req, res) => { // POST /user/
     
