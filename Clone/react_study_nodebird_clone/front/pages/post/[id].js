1fb8fddf23f852bcd4bfb30c89dd505581417a83
jongwon <54282927+loosie@users.noreply.github.com>
2021-02-09 22:25:34 +0900

    #55 getStaticPaths 적용해보기

--- Clone/react_study_nodebird_clone/front/pages/post/[id].js
+++ Clone/react_study_nodebird_clone/front/pages/post/[id].js
@@ -18,6 +18,9 @@ const Post = () =>  {
     const { id } = router.query;
     const { singlePost } = useSelector((state) => state.post);
 
+    // if(router.isFallback){
+    //     return <div> 로딩중... </div>
+    // }
     return (
         <AppLayout>
             <Head>
@@ -35,6 +38,18 @@ const Post = () =>  {
         </AppLayout>
     );
 };
+
+// export async function getStaticPaths(){
+//     return{
+//         paths:[
+//             { params: { id : '1' }},
+//             { params: { id : '2' }},
+//             { params: { id : '25' }},
+
+//         ],
+//         fallback: true,
+//     };
+// }
 
 export const getServerSideProps = wrapper.getServerSideProps(async (context) =>{
     const cookie = context.req ? context.req.headers.cookie : '';
