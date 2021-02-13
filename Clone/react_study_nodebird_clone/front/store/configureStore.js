8dea570f47ba3e895fb9fc1ddd921e192d8cfaee
jongwon <54282927+loosie@users.noreply.github.com>
2021-02-14 03:44:13 +0900

    update: redux config 수정정

--- Clone/react_study_nodebird_clone/front/store/configureStore.js
+++ Clone/react_study_nodebird_clone/front/store/configureStore.js
@@ -7,10 +7,15 @@ import reducer from '../reducers';
 import rootSaga from '../sagas';
 
 
-const configureStore = (context) => {
-    // console.log(context);
+const loggerMiddleware = ({ dispatch, getState }) => (next) => (action) => {
+    console.log(action);
+    return next(action);
+};
+
+
+const configureStore = () => {
     const sagaMiddleware = createSagaMiddleware();
-    const middlewares = [sagaMiddleware];
+    const middlewares = [sagaMiddleware, loggerMiddleware];
 
     const enhancer = process.env.NODE_ENV === 'production'
     ? compose(applyMiddleware(...middlewares))
